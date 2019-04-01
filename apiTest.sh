b4faeb889d33790e9dd64fceaffa195063f12018
Sushant Bansal <sushantbansal2@gmail.com>
2019-04-01 01:54:55 -0500

    Fixed Tests

--- apiTest.sh
+++ apiTest.sh
@@ -1,7 +1,7 @@
-curl -d '{"Borough":"Test1", "Status":"OK", "ConstructionYear":345, "Height":234, "Type":"Nice"}' -H "Content-Type: application/json" -X POST http://localhost:4018/addBuilding >> TestRes.json
+curl -d '{"Borough":"Test", "Status":"OK", "ConstructionYear":346, "Height":234, "Type":"Nice"}' -H "Content-Type: application/json" -X POST http://localhost:4018/addBuilding >> TestRes.json
 
 curl http://localhost:4018/statHeightByType >> TestRes.json
 
-curl http://localhost:4018/buildings >> TestRes.json
+curl http://localhost:4018/getBuildings >> TestRes.json
 
 
