2cc3d89ad5557a819f43bfc0886bb695a7e337d5
Sushant Bansal <sushantbansal2@gmail.com>
2019-04-03 01:23:06 -0500

    Updated

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 )
 
 func main() {
 
-	log.Println("Starting ETL Section")
-
 	var buildings []Building
-	/* Getting Data From the Open Data API */
-	extract(&buildings)
-	/* Transforming and Loading into a local instance of MongoDB*/
-	trnsfrmLd(buildings)
-
-	log.Println("Successfully Added Entries to Database.")
-	log.Println("Ending ETL Section")
-
+	var etlCheck int
+	fmt.Println("Press 1 to start ETL process and 0 to skip!")
+	_, err := fmt.Scan(&etlCheck)
+	if err != nil {
+		fmt.Println(err.Error(), "Please re-run")
+	}
+	if etlCheck != 0 {
+		log.Println("Starting ETL Section")
+		/* Getting Data From the Open Data API */
+		extract(&buildings)
+		/* Transforming and Loading into a local instance of MongoDB*/
+		trnsfrmLd(buildings)
+		log.Println("Successfully Added Entries to Database.")
+		log.Println("Ending ETL Section")
+	}
 	/* REST API */
 
 	log.Println("Starting Rest-API")
