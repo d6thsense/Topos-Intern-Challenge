f1e9c9abfd831cef290976cd43f02d75d2f9d6b7
Sushant Bansal <sushantbansal2@gmail.com>
2019-04-03 00:19:53 -0500

    Added Delete Test

--- apiTest.sh
+++ apiTest.sh
@@ -1,7 +1,7 @@
-curl -d '{"Borough":"Test", "Status":"OK", "ConstructionYear":346, "Height":234, "Type":"Nice"}' -H "Content-Type: application/json" -X POST http://localhost:4018/addBuilding >> TestRes.json
+#curl -d '{"Borough":"Test", "Status":"OK", "ConstructionYear":346, "Height":234, "Type":"Nice"}' -H "Content-Type: application/json" -X POST http://localhost:4018/addBuilding >> TestRes.json
 
-curl http://localhost:4018/statHeightByType >> TestRes.json
+#curl http://localhost:4018/statHeightByType >> TestRes.json
 
-curl http://localhost:4018/getBuildings >> TestRes.json
+#curl http://localhost:4018/getBuildings >> TestRes.json
 
-
+curl -X "DELETE" http://localhost:4018/removeBuilding/5ca41979ba6d3392e73be3d3
